f4788c4b0c434ea19bf070dfa1d89300611fb81d
João Sousa-Pinto <joaospinto@gmail.com>
2026-07-13 13:41:13 -0700

    Tighten CUTEst numerical stall threshold

--- problem_definitions/cutest_problems/runner.cpp
+++ problem_definitions/cutest_problems/runner.cpp
@@ -26,6 +26,7 @@ auto run(const char *runtime_path, const char *problem_library_path,
   settings.line_search.skip_line_search = false;
   settings.line_search.max_iterations = 5000;
   settings.regularization.max_attempts = 24;
+  settings.termination.max_merit_slope = 1e-24;
   if (use_qp_settings) {
     settings.barrier.mu_update_factor = 0.2;
     settings.penalty.scale_violation_reduction_with_step_size = true;
